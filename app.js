c715f6c4fa125c8eec85ca09f48eb7b5f80326d3
pongsakorn khumphis <pongsakorn.k@ajthai.com>
2023-11-01 17:06:03 +0700

    add file

--- app.js
+++ app.js
@@ -110,3 +110,20 @@ dateInput.addEventListener("keyup", (e)=>{
         }
     }
 });
+
+ 
+gotoBtn.addEventListener('click', gotoDate);
+
+function gotoDate(){
+    const dateArr = dateInput.value.split("/");
+    console.log(dateArr)
+    if(dateArr.length == 2){
+        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length == 4){
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    alert("invalid date");
+}
